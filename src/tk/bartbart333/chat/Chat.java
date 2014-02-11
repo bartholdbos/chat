c03f02170961ec053f0d695984eaea34f121fda4
SteFilms <s.siekman@gmail.com>
2014-02-11 16:20:56 +0100

    Packter class beginning
    Beginning of the packet class

--- src/tk/bartbart333/chat/Chat.java
+++ src/tk/bartbart333/chat/Chat.java
@@ -1,12 +1,19 @@
 package tk.bartbart333.chat;
 
-import tk.bartbart333.chat.ui.Window;
+import tk.bartbart333.udp.Packet;
 
 public class Chat {
 	
 	public static void main(String[] args) {
-		Window window = new Window();
-		window.show();
+		byte[] buffer = new byte[1024];
+		buffer[0] = 'h';
+		buffer[1] = 'e';
+		buffer[2] = 'l';
+		buffer[3] = 'l';
+		buffer[4] = 'o';
+		Packet packet = new Packet(buffer, 5);
+		//Window window = new Window();
+		//window.show();
 	}
 	
 }
--- src/tk/bartbart333/udp/Packet.java
+++ src/tk/bartbart333/udp/Packet.java
@@ -2,4 +2,8 @@ package tk.bartbart333.udp;
 
 public class Packet {
 	
+	public Packet(byte[] buffer, int length) {
+		String s = new String(buffer, 0, length);
+	}
+	
 }

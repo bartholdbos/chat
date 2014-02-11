683e3fe40e456562ec948bd24e418d4d918218c3
SteFilms <s.siekman@gmail.com>
2014-02-11 16:12:54 +0100

    Window start
    Beginning of the window

--- src/tk/bartbart333/chat/Chat.java
+++ src/tk/bartbart333/chat/Chat.java
@@ -1,5 +1,12 @@
 package tk.bartbart333.chat;
 
+import tk.bartbart333.chat.ui.Window;
+
 public class Chat {
+	
+	public static void main(String[] args) {
+		Window window = new Window();
+		window.show();
+	}
 	
 }
--- src/tk/bartbart333/chat/ui/Window.java
+++ src/tk/bartbart333/chat/ui/Window.java
@@ -1,5 +1,57 @@
 package tk.bartbart333.chat.ui;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class Window {
 
+	private JFrame frame;
+	private JPanel panel;
+	private JButton button;
+	
+	public Window() {
+		frame = new JFrame("Chat Application");
+		
+		panel = new ContentPanel();
+		panel.setPreferredSize(new Dimension(800, 600));
+		panel.setLayout(null);
+		initUI();
+		frame.setContentPane(panel);
+		
+		frame.pack();
+		frame.setMinimumSize(frame.getSize());
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+	}
+	
+	private void initUI() {
+		panel.setBackground(new Color(255, 255, 255));
+		
+		button = new JButton("Send");
+		button.setBounds(100, 100, 100, 25);
+		panel.add(button);
+	}
+	
+	public void show() {
+		frame.setVisible(true);
+	}
+	
+	private class ContentPanel extends JPanel {
+		
+		public void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			
+			g.clearRect(0, 0, getWidth(), getHeight());
+			g.drawRect(getWidth() / 2 - 50, getHeight() / 2 - 50, 100, 100);
+			button.setBounds(getWidth() / 2 - 40, getHeight() / 2 - 12, 80, 24);
+		}
+		
+	}
+	
 }
